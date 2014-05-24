0f733982cd014fe886c2b629e35ebd8e5705dc8e
emaszs <phonixreal@gmail.com>
2014-05-24 20:53:58 +0300

    Basic resource implementation

--- src/res/HardDriveMemory.java
+++ src/res/HardDriveMemory.java
@@ -1,6 +1,10 @@
 package res;
+
+
 //TODO
 public class HardDriveMemory extends Resource {
+	
+	int fileId;
 
 	public HardDriveMemory(int intID, String extID, int creatorProcess) {
 		super(intID, extID, creatorProcess);
--- src/res/TaskInHardDrive.java
+++ src/res/TaskInHardDrive.java
@@ -2,6 +2,8 @@ package res;
 //TODO
 public class TaskInHardDrive extends Resource {
 
+	int fileWithTaskId;
+	
 	public TaskInHardDrive(int intID, String extID, int creatorProcess) {
 		super(intID, extID, creatorProcess);
 		
