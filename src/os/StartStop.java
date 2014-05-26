2b2baa4744222537a473542361737b07086a98be
Djtomulis <tomas.orvidas@gmail.com>
2014-05-26 22:33:39 +0300

    Updated StartStop

--- src/os/StartStop.java
+++ src/os/StartStop.java
@@ -1,4 +1,7 @@
 package os;
+
+import res.Resource;
+
 //TODO
 public class StartStop extends Process {
 
@@ -14,9 +17,17 @@ public class StartStop extends Process {
 			
 			
 			
-			//System process initialization
 			
-		
+			
+			//System process initialization
+			PyOS.createProcess(2, intID, 80, null);
+			PyOS.createProcess(3, intID, 90, null);
+			PyOS.createProcess(4, intID, 70, null);
+			PyOS.createProcess(5, intID, 60, null);
+			PyOS.createProcess(6, intID, 55, null);
+			PyOS.createProcess(7, intID, 54, null);
+			PyOS.createProcess(8, intID, 50, null);
+			PyOS.createProcess(9, intID, 45, null);
 			PyOS.askForResource(PyOS.waitingList1, 1);
 			neededResource = 1;	
 			phase = 1;
@@ -30,7 +41,10 @@ public class StartStop extends Process {
 			
 			
 			
-			
+			//Process deletion
+			while (PyOS.processList.size()>1) {
+				PyOS.deleteProcess(PyOS.processList.get(1).intID, PyOS.findChildrenListIndex(PyOS.processList.get(1).intID));
+			}
 			PyOS.MOSEnd = 1;
 		}
 	}
