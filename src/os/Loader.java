7013b85707dc6c64fbe71f2523dffca8775e7313
emaszs <phonixreal@gmail.com>
2014-05-26 15:13:12 +0300

    Loader kinda functional

--- src/os/Loader.java
+++ src/os/Loader.java
@@ -1,5 +1,11 @@
 package os;
 
+
+import res.HardDriveMemory;
+import machine.Memory;
+import memory.Hdd;
+import memory.LoaderHelper;
+
 //TODO
 public class Loader extends Process {
 	
@@ -36,9 +42,54 @@ public class Loader extends Process {
 		//TODO
 		if ((phase == 3) && (receivedResource == 3) && (pState.equals("ru"))) {	
 
-			
-			//Nuspresk, ar Task in user memory tures svarios info
-			//PyOS.freeResource(PyOS.waitingList9, 9, ownedResList.get(0));
+			int VMProgramIC = 0;
+			int fileNum = ((HardDriveMemory)PyOS.findResource(PyOS.currentProcess.ownedResList, "HardDriveMemory")).fileId;
+			String line = new String();
+			while ((line = Hdd.readFromFile(fileNum)) != null) {
+
+				if (LoaderHelper.isLegal(line)) {
+
+					/*
+					 * DSXY - data segment allocation. XY - virtual adress to
+					 * store data
+					 */
+					if (line.matches("DS\\d\\d")) {
+						int dataSegIC = 0;
+						int adr = Integer.parseInt(line.substring(2, 4));
+						line = Hdd.readFromFile(fileNum); // Skip DSXY line
+						while (!line.matches("DSOV")) {
+							Memory.writeToVirtualAddress(adr + dataSegIC, line);
+							dataSegIC++;
+							line = Hdd.readFromFile(fileNum);
+						}
+						line = Hdd.readFromFile(fileNum); // Skip DSOV line
+					}
+
+					if (line != null && line.matches("\\$STR")) {
+						line = Hdd.readFromFile(fileNum);
+					}
+
+					if (line != null && line.matches("\\$END")) {
+						break;
+					}
+					/*
+					 * Checking once more if line isn't empty Required if Data
+					 * Segment is at the end of the input flash drive.
+					 */
+					if (line != null) {
+						Memory.writeToVirtualAddress(VMProgramIC, line);
+						VMProgramIC++;
+					}
+				} else {
+					System.out
+							.println("Illegal syntax found! stopping loading.");
+					System.out.println(line);
+					break;
+				}
+			}
+			//Nuspresk, ar Task in user memory tures svarbios info
+			//Netures
+			PyOS.freeResource(PyOS.waitingList9, 9, ownedResList.get(0));
 			neededResource = 0;
 			phase = 4;
 		}
--- src/res/HardDriveMemory.java
+++ src/res/HardDriveMemory.java
@@ -4,7 +4,7 @@ package res;
 //TODO
 public class HardDriveMemory extends Resource {
 	
-	int fileId;
+	public int fileId;
 
 	public HardDriveMemory(int intID, String extID, int creatorProcess) {
 		super(intID, extID, creatorProcess);
